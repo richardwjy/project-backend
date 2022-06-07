ca00ef4d0894e08c6ca1b62a120f84aa6b285249
Richard <richard_huang0309@yahoo.com>
2022-06-07 16:01:00 +0700

    change code

--- server.js
+++ server.js
@@ -9,32 +9,44 @@ log4js.configure({
     categories: { default: { appenders: ["auth"], level: "debug" } }
 });
 
-const webRoute = require('./web/router');
+// const webRoute = require('./web/router');
 app.use(cors({
     credentials: true,
     origin: true
     // allowedHeaders: ['X-Requested-With', 'X-HTTP-Method-Override', 'Content-Type', 'Accept']
 }))
 
-const oracledb = require('oracledb');
-const fs = require('fs');
+// const oracledb = require('oracledb');
+// const fs = require('fs');
 
-let libPath;
-if (process.platform == 'win32') {
-    libPath = 'C:\\oracle\\instantclient_21_3'
-} else if (process.platform === 'linux') {
-    libPath = './instantclient_21_5'
-}
+// let libPath;
+// if (process.platform == 'win32') {
+//     libPath = 'C:\\oracle\\instantclient_21_3'
+// } else if (process.platform === 'linux') {
+//     libPath = './instantclient_21_5'
+// }
 
-if (libPath && fs.existsSync(libPath)) {
-    oracledb.initOracleClient({ libDir: libPath });
-}
+// if (libPath && fs.existsSync(libPath)) {
+//     oracledb.initOracleClient({ libDir: libPath });
+// }
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/v1/api', webRoute);
+// app.use('/v1/api', webRoute);
+
+app.get('/', (req, res) => {
+    return res.json({ message: "Hello World" });
+})
+
+app.get('/home', (req, res) => {
+    return res.json({ message: "Home page!" });
+})
+
+app.get('/about', (req, res) => {
+    return res.json({ message: "About page!" });
+})
 
 app.listen(PORT, () => {
     console.log(`Application is running on PORT : ${PORT}`);
